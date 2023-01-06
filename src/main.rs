a7d3bc7d0f33dafe54ed4c614ec5c4f943ad0502
Alec Chen <alecchendev@gmail.com>
2023-01-05 20:21:24 -0600

    Parameterize creating and deleting stream rules for single user

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
+use std::backtrace::BacktraceStatus;
 use std::collections::HashMap;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
 
 use hyper::client::HttpConnector;
-use hyper_tls::HttpsConnector;
-use hyper::{Client, Request, Body, Method, Uri};
 use hyper::header::{HeaderValue, AUTHORIZATION};
+use hyper::{Body, Client, Method, Request, Uri};
+use hyper_tls::HttpsConnector;
 
+use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
-use serde::{Serialize, Deserialize};
 
 use std::env;
 
@@ -32,26 +33,39 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let arg = args.get(1).unwrap_or(&empty_str);
 
     match arg.as_str() {
-        "get_stream" => get_stream(&client, bearer_token).await.and_then(|_| Ok(()))?,
-        "get_rules" => get_stream_rules(&client, bearer_token).await.and_then(|_| Ok(()))?,
+        "get_stream" => get_stream(&client, bearer_token)
+            .await
+            .and_then(|_| Ok(()))?,
+        "get_rules" => get_stream_rules(&client, bearer_token)
+            .await
+            .and_then(|_| Ok(()))?,
         // "create_stream_rule" => create_stream_rule(&client, bearer_token).await?,
-        "create_greg" => create_only_greg_stream_rule(&client, bearer_token).await?,
+        "create_greg" => {
+            create_only_user_stream_rule(&client, bearer_token, "greg16676935420").await?
+        }
+        "create_alec" => create_only_user_stream_rule(&client, bearer_token, "alecchendev").await?,
         // "delete_stream_rule" => delete_stream_rule(&client, bearer_token, vec!["1611155413902180354".to_string()]).await?,
-        "delete_greg" => delete_only_greg_stream_rule(&client, bearer_token).await?,
+        "delete_greg" => {
+            delete_only_user_stream_rule(&client, bearer_token, "greg16676935420").await?
+        }
+        "delete_alec" => delete_only_user_stream_rule(&client, bearer_token, "alecchendev").await?,
         _ => println!("No command found"),
     };
 
     Ok(())
 }
 
-async fn get_stream(client: &Client<HttpsConnector<HttpConnector>>, bearer_token: &str) -> Result<Value, Box<dyn Error>> {
+async fn get_stream(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    bearer_token: &str,
+) -> Result<Value, Box<dyn Error>> {
     let req = Request::builder()
         .method(Method::GET)
         .uri("https://api.twitter.com/2/tweets/search/stream")
         .header("Content-Type", "application/json")
         .header("Authorization", "Bearer ".to_owned() + bearer_token)
         .body(Body::empty())?;
-    
+
     let res: hyper::Response<Body> = client.request(req).await?;
 
     println!("status: {}", res.status());
@@ -65,14 +79,17 @@ async fn get_stream(client: &Client<HttpsConnector<HttpConnector>>, bearer_token
     Ok(json)
 }
 
-async fn get_stream_rules(client: &Client<HttpsConnector<HttpConnector>>, bearer_token: &str) -> Result<Value, Box<dyn Error>> {
+async fn get_stream_rules(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    bearer_token: &str,
+) -> Result<Value, Box<dyn Error>> {
     let req = Request::builder()
         .method(Method::GET)
         .uri("https://api.twitter.com/2/tweets/search/stream/rules")
         .header("Content-Type", "application/json")
         .header("Authorization", "Bearer ".to_owned() + bearer_token)
         .body(Body::empty())?;
-    
+
     let res = client.request(req).await?;
 
     println!("status: {}", res.status());
@@ -93,50 +110,44 @@ struct Rule {
     tag: String,
 }
 
-async fn delete_only_greg_stream_rule(client: &Client<HttpsConnector<HttpConnector>>, bearer_token: &str) -> Result<(), Box<dyn Error>> {
-
+async fn delete_only_user_stream_rule(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    bearer_token: &str,
+    user: &str,
+) -> Result<(), Box<dyn Error>> {
     let res = get_stream_rules(&client, bearer_token).await?;
 
-    let data = res["data"].as_array().expect("stream rules data is not an array");
+    let data = res["data"]
+        .as_array()
+        .expect("stream rules data is not an array");
 
-    let rules: Vec<Rule> = data.iter().map(|rule| {
-        serde_json::from_value(rule.clone()).expect("could not deserialize rule")
-    }).collect();
+    let rules: Vec<Rule> = data
+        .iter()
+        .map(|rule| serde_json::from_value(rule.clone()).expect("could not deserialize rule"))
+        .collect();
 
-    let only_greg_id: Option<String> = rules.iter().find(|rule| rule.tag == "from greg").map(|rule| rule.id.clone());
+    let only_user_id: Option<String> = rules
+        .iter()
+        .find(|rule| rule.tag == format!("from {}", user))
+        .map(|rule| rule.id.clone());
 
-    let payload = json!({
-        "delete": {
-            "ids": vec![only_greg_id]
-        }
-    }).to_string();
-    
-    let req = Request::builder()
-        .method(Method::POST)
-        .uri("https://api.twitter.com/2/tweets/search/stream/rules")
-        .header("Content-Type", "application/json")
-        .header("Authorization", "Bearer ".to_owned() + bearer_token)
-        .body(Body::from(payload.to_string()))?;
-
-    let res = client.request(req).await?;
-
-    println!("status: {}", res.status());
-
-    let buf = hyper::body::to_bytes(res).await?;
-
-    println!("body: {:?}", buf);
+    delete_stream_rule(client, bearer_token, vec![only_user_id.unwrap()]).await?;
 
     Ok(())
 }
 
-async fn delete_stream_rule(client: &Client<HttpsConnector<HttpConnector>>, bearer_token: &str, ids: Vec<String>) -> Result<(), Box<dyn Error>> {
-
+async fn delete_stream_rule(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    bearer_token: &str,
+    ids: Vec<String>,
+) -> Result<(), Box<dyn Error>> {
     let payload = json!({
         "delete": {
             "ids": ids
         }
-    }).to_string();
-    
+    })
+    .to_string();
+
     let req = Request::builder()
         .method(Method::POST)
         .uri("https://api.twitter.com/2/tweets/search/stream/rules")
@@ -155,16 +166,21 @@ async fn delete_stream_rule(client: &Client<HttpsConnector<HttpConnector>>, bear
     Ok(())
 }
 
-async fn create_only_greg_stream_rule(client: &Client<HttpsConnector<HttpConnector>>, bearer_token: &str) -> Result<(), Box<dyn Error>> {
+async fn create_only_user_stream_rule(
+    client: &Client<HttpsConnector<HttpConnector>>,
+    bearer_token: &str,
+    user: &str,
+) -> Result<(), Box<dyn Error>> {
     let payload = json!({
         "add": [
             {
-                "value": "from:greg16676935420",
-                "tag": "from greg"
+                "value": format!("from:{}", user),
+                "tag": format!("from {}", user)
             }
-        ] 
-    }).to_string();
-    
+        ]
+    })
+    .to_string();
+
     let req = Request::builder()
         .method(Method::POST)
         .uri("https://api.twitter.com/2/tweets/search/stream/rules")
