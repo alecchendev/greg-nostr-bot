38d07a9540dc73a7371a0a3d8f71eec48c28780c
Alec Chen <alecchendev@gmail.com>
2023-01-06 12:24:10 -0600

    Implement publishing an event

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
@@ -8,23 +9,40 @@ use hyper::client::HttpConnector;
 use hyper::{Body, Client, Method, Request};
 use hyper_tls::HttpsConnector;
 
+// use futures::StreamExt;
+use futures_util::sink::SinkExt;
 use serde::{Deserialize, Serialize};
 use serde_json::{json, Value};
+use tokio::net::TcpStream;
+use tokio_tungstenite::{
+    connect_async, tungstenite::Message, Connector, MaybeTlsStream, WebSocketStream,
+};
 
-use std::env;
+// use nostr_rust;
+use nostr_types::{Event, EventKind, PreEvent, PrivateKey, PublicKey, Unixtime};
+
+use tokio::sync::{Mutex, MutexGuard};
+
+use std::sync::Arc;
+
+use hyper::Uri;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     println!("Hello, world!");
 
-    let _relays = read_relays()?;
+    let relays = read_relays()?;
 
     let env = read_env()?;
     let _api_key = env.get("API_KEY").unwrap();
     let bearer_token = env.get("BEARER_TOKEN").unwrap();
+    let private_key = PrivateKey::try_from_bech32_string(env.get("PRIVATE_KEY").unwrap())?;
 
     let https = HttpsConnector::new();
     let client = Client::builder().build::<_, hyper::Body>(https);
+
+    let mut nostr_client = NostrClient::new().await;
+    nostr_client.add_relays(&relays).await;
 
     // Cli
     let args: Vec<String> = env::args().collect();
@@ -48,10 +66,84 @@ async fn main() -> Result<(), Box<dyn Error>> {
             delete_only_user_stream_rule(&client, bearer_token, "greg16676935420").await?
         }
         "delete_alec" => delete_only_user_stream_rule(&client, bearer_token, "alecchendev").await?,
+        "post" => post_note(&mut nostr_client, "Hello, nostr!", &private_key).await?,
+        // "get_notes" => get_notes(),
         _ => println!("No command found"),
     };
 
     Ok(())
+}
+
+async fn post_note(
+    nostr_client: &mut NostrClient,
+    text: &str,
+    private_key: &nostr_types::PrivateKey,
+) -> Result<(), Box<dyn Error>> {
+    let pre_event = PreEvent {
+        /// The public key of the actor who is creating the event
+        pubkey: private_key.public_key(),
+        /// The time at which the event was created
+        created_at: Unixtime(
+            std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_secs() as i64,
+        ),
+        /// The kind of event
+        kind: EventKind::TextNote,
+        /// A set of tags that apply to the event
+        tags: Vec::new(),
+        /// The content of the event
+        content: text.to_string(),
+        /// An optional verified time for the event (using OpenTimestamp)
+        ots: None,
+    };
+    let event = Event::new(pre_event, private_key)?;
+    assert!(event.verify(None).is_ok());
+    nostr_client.publish_event(&event).await?;
+    Ok(())
+}
+
+struct NostrClient {
+    relays: HashMap<String, Arc<Mutex<WebSocketStream<MaybeTlsStream<TcpStream>>>>>,
+}
+
+impl NostrClient {
+    async fn new() -> Self {
+        Self {
+            relays: HashMap::new(),
+        }
+    }
+
+    async fn add_relays(&mut self, relay_urls: &Vec<String>) -> Result<(), Box<dyn Error>> {
+        for url in relay_urls {
+            self.add_relay(url).await?;
+        }
+        Ok(())
+    }
+
+    async fn add_relay(&mut self, url: &String) -> Result<(), Box<dyn Error>> {
+        if self.relays.contains_key(url) {
+            return Ok(());
+        }
+        let uri = url.parse::<Uri>().unwrap();
+        let (ws_stream, response) = connect_async(uri).await?;
+        println!("Relay: {} | connect response: {:?}", url, response);
+        self.relays.insert(url.clone(), Arc::new(Mutex::new(ws_stream)));
+        Ok(())
+    }
+
+    async fn publish_event(&mut self, event: &Event) -> Result<(), Box<dyn Error>> {
+        let event_json = json!(["EVENT", event]).to_string();
+        let message = Message::text(event_json.clone());
+        for (url, ws_stream) in self.relays.iter() {
+            let mut ws_stream = ws_stream.lock().await;
+
+            ws_stream.send(message.clone()).await?;
+            println!("Relay: {} | sent event: {:?}", url, event);
+        }
+        Ok(())
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -105,9 +197,8 @@ fn handle_tweet(s: String) -> Result<(), Box<dyn Error>> {
     let data: TweetData = serde_json::from_value(json["data"].clone())?;
 
     println!("data: {:?}", data);
-    
+
     // post data.text to nostr!
-    
 
     Ok(())
 }
